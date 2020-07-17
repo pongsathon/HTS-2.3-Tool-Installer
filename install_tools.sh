6a3a79902a1542ddedb83dd57692e74580c0b829
pongsathon janyoi <pongsathon.janyoi@gmail.com>
2020-07-17 17:00:35 +0700

    first commit

---
+++ install_tools.sh
@@ -0,0 +1,50 @@
+#!bin/bash
+
+#run this command before run this script
+#sudo apt install csh
+
+bin_dir=$(pwd)/bin
+tmp_dir=tmp
+rm -rf $tmp_dir
+mkdir -p $tmp_dir
+
+src_dir=$(pwd)/src
+tar -zxvf $src_dir/HTK-3.4.1.tar.gz --directory tmp
+tar -zxvf $src_dir/HDecode-3.4.1.tar.gz --directory tmp
+tar -xvjf $src_dir/HTS-2.3_for_HTK-3.4.1.tar.bz2 --directory tmp
+
+mkdir -p $bin_dir/hts-2.3
+cd tmp/htk/
+patch -p1 -d . < ../HTS-2.3_for_HTK-3.4.1.patch
+./configure CFLAGS="-DARCH=linux" --prefix=$bin_dir/hts-2.3
+make install
+make hdecode
+make install-hdecode
+
+cd ../..
+
+mkdir -p $bin_dir/sptk-3.10
+tar -zxvf $src_dir/SPTK-3.10.tar.gz --directory tmp
+cd tmp/SPTK-3.10
+./configure --prefix=$bin_dir/sptk-3.10
+make install
+
+cd ../..
+
+mkdir -p $bin_dir/hts_engine_1.10
+tar -zxvf $src_dir/hts_engine_API-1.10.tar.gz --directory tmp
+cd tmp/hts_engine_API-1.10
+./configure --prefix=$bin_dir/hts_engine_1.10
+make install
+
+cd $src_dir/WORLD;
+make
+make analysis synth
+make clean
+
+mkdir $bin_dir/WORLD
+cp $src_dir/WORLD/build/analysis $bin_dir/WORLD
+cp $src_dir/WORLD/build/synth $bin_dir/WORLD
+
+echo "Succeed."
+
